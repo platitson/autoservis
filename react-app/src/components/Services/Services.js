76054ec0f206cbe82b5c0fcff2b72467ee13e978
Yuliya Platsitsyna <yuliya.platsitsyna@gmail.com>
2021-09-05 15:36:39 +0200

    add Services

--- react-app/src/components/Services/Services.js
+++ react-app/src/components/Services/Services.js
@@ -1,7 +1,12 @@
-function Services() {
-    return <div>
+import React from 'react';
+import styles from './Services.module.scss';
 
-    </div>
+class Services extends React.Component {
+    render() {
+        return <div className={styles.services}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at libero sit amet velit congue eleifend. Nam et magna ac lorem pellentesque vehicula non sit amet odio. Ut facilisis nisl sit amet mattis pharetra. Donec vel arcu purus. Vestibulum quis venenatis nulla. Maecenas interdum sapien non cursus dictum. Nunc accumsan nibh in arcu luctus ornare. Aliquam rhoncus elit felis, ac tristique lorem posuere varius. Nam euismod enim non nunc condimentum, quis pellentesque metus iaculis. Phasellus vitae lectus a lacus ultrices pellentesque sed ut urna. Nulla facilisi. Suspendisse facilisis scelerisque ante, id sollicitudin mauris tincidunt ac. Cras et aliquet sapien. Suspendisse quam justo, posuere mollis turpis eget, volutpat tristique orci.</p>
+        </div>
+    }
 }
 
 export default Services;
